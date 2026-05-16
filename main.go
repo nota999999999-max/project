ce7312a0ec5e16c507ef09fd9511ef6a9e9f794c
natalya finaeva <nota999999999@gmail.com>
2026-05-16 12:26:44 +0430

    Update main.go

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	r.HandleFunc("/", DroperHandler).Methods("GET")
 	r.HandleFunc("/mobile", MobileNumber).Methods("GET")
 	r.HandleFunc("/card", UCard).Methods("GET")
+	http.HandleFunc("/transfer", TransferCriptoToCard)
 	r.HandleFunc("/droper", Dropers).Methods("GET")
 	fmt.Println("List")
 	http.ListenAndServe(":8080", r)
@@ -51,12 +52,11 @@ func DroperHandler(w http.ResponseWriter, r *http.Request) {
 
 func UsersList(w http.ResponseWriter, r *http.Request) {
 	users := [5]User{
-		{Name: "Muhammadjon S", MobileNumber, Card: 9876543212345678},
-		{Name: "Parviz H", MobileNumber, Card: 1234567898765432},
-		{Name: "Zarina A", MobileNumber, Card: 1111222233334444},
-		{Name: "Bezhan Sh", MobileNumber, Card: 5555666677778888},
-		{Name: "Guldofarin Kh", MobileNumber, Card: 0000111122223333},
-
+		{Name: "Muhammadjon S", MobileNumber: +992000111111, Card: 9876543212345678},
+		{Name: "Parviz H", MobileNumber: +992000222222, Card: 1234567898765432},
+		{Name: "Zarina A", MobileNumber: +998000333333, Card: 1111222233334444},
+		{Name: "Bezhan Sh", MobileNumber: +992000444444, Card: 5555666677778888},
+		{Name: "Guldofarin Kh", MobileNumber: +7000555555, Card: 0000111122223333},
 	}
 	fmt.Println(users)
 }
@@ -77,22 +77,22 @@ func MobileNumber(w http.ResponseWriter, r *http.Request) {
 func UCard(w http.ResponseWriter, r *http.Request) {
 	var visa Card
 	VList := []Card{
-		{CardNumber: 1234567898765432, CardType: "visa", User},
-		{CardNumber: 1111222233334444, CardType: "visa", User},
-		{CardNumber: 5555666677778888, CardType: "visa", User},
-		{CardNumber: 0000111122223333, CardType: "visa", User},
+		{CardNumber: 1234567898765432, Format: "visa", User},
+		{CardNumber: 1111222233334444, Format: "visa", User},
+		{CardNumber: 5555666677778888, Format: "visa", User},
+		{CardNumber: 0000111122223333, Format: "visa", User},
 	}
 
 	VTList := []transactions{
-		{ID: 001, Amount: 150.75, Currency: "USD", Type: "in"},
-		{ID: 002, Amount: 320.00, Currency: "TJS", Type: "in"},
-		{ID: 003, Amount: 89.99, Currency: "TJS", Type: "out"}
+		{ID: 001, Amount: 150.75, Currency: "USD", Type: "in", Format:},
+		{ID: 002, Amount: 320.00, Currency: "TJS", Type: "in", Format:},
+		{ID: 003, Amount: 89.99, Currency: "TJS", Type: "out", Format:},
 	}
 	visa.Transactions = VTList
 
-	var км Card
+	var km Card
 	KMTList := []Card{
-		{CardNumber: 9876543212345678, CardType: "км", User},
+		{CardNumber: 9876543212345678, Format: "km", User},
 	}
 
 	KMTList := []transactions{
@@ -106,10 +106,44 @@ func UCard(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func TransferCriptoToCard(w http.ResponseWriter, r *http.Request) {
+	var cripto Coin
+
+	type Coin struct {
+	Name       string  //BTC
+	Amount     float64  //
+	WalletAdress string //bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh
+}
+
+Transfer := []transactions{
+{ID: 001, Amount: 500.00, Currency: "TJS", Type: "in"}
+}
+
+type km struct{
+	PrefixBIN int
+	KMNumberNumber int
+}
+
+kmNumber := []km{
+	{6278, KMNumber},
+	{5152, KMNumber},
+	{5440, KMNumber},
+}
+
+//if 6278, 5152, 5440
+
+if km(PrefixBIN) {
+		http.Error(w, "Wrong Card Number", http.StatusBadRequest)
+		return
+	}
+}
+
+
 func Dropers(w http.ResponseWriter, r *http.Request) {
+
 	users := [2]Droper{
-		{Name: "Parviz H", Mobile_number: "+992", Card_number: 1234567898765432},
-		{Name: "Bezhan Sh", Mobile_number: "+992", Card_number: 5555666677778888},
+		{CardNumber: 1234567898765432},
+		{CardNumber: 5555666677778888},
 	}
 	return Dropers
 }
