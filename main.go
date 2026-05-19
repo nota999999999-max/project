62ad4882f2cb80a789fc7dfdf44de231c884413a
natalya finaeva <nota999999999@gmail.com>
2026-05-19 12:10:15 +0430

    update main

--- main.go
+++ main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+func main() {
+
+	http.HandleFunc("/", DropCardHandler)
+	http.HandleFunc("/users", UsersList)
+	http.HandleFunc("/mobile", MobileNumber)
+	http.HandleFunc("/card", UCard)
+	http.HandleFunc("/transfer", TransferCriptoToCard)
+	http.HandleFunc("/dropcard", DropCard)
+
+	fmt.Println("Вывод на :8080")
+	http.ListenAndServe(":8080", nil)
+
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", DropCardHandler).Methods("GET")
+	r.HandleFunc("/user", UsersList).Methods("POST")
+	r.HandleFunc("/mobile", MobileNumber).Methods("GET")
+	r.HandleFunc("/card", UCard).Methods("GET")
+	http.HandleFunc("/transfer", TransferCriptoToCard)
+	r.HandleFunc("/dropcard", DropCard).Methods("GET")
+
+	fmt.Println("List")
+	http.ListenAndServe(":8080", r)
+}
+
 
 type User struct {
 	Name         string `json:"name"`
@@ -13,11 +41,45 @@ type User struct {
 	Card         []Card
 }
 
+func DropCardHandler(w http.ResponseWriter, r *http.Request) {
+	
+	user := User{
+		Name: "name",
+}
+	w.Write([]byte("DList"))
+}
+
+
+func UsersList(w http.ResponseWriter, r *http.Request) {
+	users := [5]User{
+		{Name: "Muhammadjon S", MobileNumber: +992000111111, Card: 9876543212345678},
+		{Name: "Parviz H", MobileNumber: +992000222222, Card: 1234567898765432},
+		{Name: "Zarina A", MobileNumber: +998000333333, Card: 1111222233334444},
+		{Name: "Bezhan Sh", MobileNumber: +992000444444, Card: 5555666677778888},
+		{Name: "Guldofarin Kh", MobileNumber: +7000555555, Card: 0000111122223333},
+	}
+	fmt.Println(users)
+}
+
 type MobileN struct {
 	Prefix   string
 	Country  int
 	Number   int
 }
+
+func MobileNumber(w http.ResponseWriter, r *http.Request) {
+	var number MobileN
+	
+	MList := []mobileN{
+		{Prefix: "+", Country: 992, Number: 000111111},
+		{Prefix: "+", Country: 992, Number: 000222222},
+		{Prefix: "+", Country: 998, Number: 000333333},
+		{Prefix: "+", Country: 992, Number: 000444444},
+		{Prefix: "+", Country: 7, Number: 000555555},
+	}
+		fmt.Println("Mobile Number:", number)
+}
+
 
 type Card struct {
 	CardNumber   int
@@ -34,45 +96,6 @@ type transactions struct {
 
 var users = make(map[string]User)
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/user", UsersList).Methods("POST")
-	r.HandleFunc("/", DroperHandler).Methods("GET")
-	r.HandleFunc("/mobile", MobileNumber).Methods("GET")
-	r.HandleFunc("/card", UCard).Methods("GET")
-	http.HandleFunc("/transfer", TransferCriptoToCard)
-	r.HandleFunc("/droper", Dropers).Methods("GET")
-	fmt.Println("List")
-	http.ListenAndServe(":8080", r)
-}
-
-func DroperHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("DList"))
-}
-
-func UsersList(w http.ResponseWriter, r *http.Request) {
-	users := [5]User{
-		{Name: "Muhammadjon S", MobileNumber: +992000111111, Card: 9876543212345678},
-		{Name: "Parviz H", MobileNumber: +992000222222, Card: 1234567898765432},
-		{Name: "Zarina A", MobileNumber: +998000333333, Card: 1111222233334444},
-		{Name: "Bezhan Sh", MobileNumber: +992000444444, Card: 5555666677778888},
-		{Name: "Guldofarin Kh", MobileNumber: +7000555555, Card: 0000111122223333},
-	}
-	fmt.Println(users)
-}
-
-func MobileNumber(w http.ResponseWriter, r *http.Request) {
-	var number MobileN
-	
-	MList := []mobileN{
-		{Prefix: "+", Country: 992, Number: 000111111},
-		{Prefix: "+", Country: 992, Number: 000222222},
-		{Prefix: "+", Country: 998, Number: 000333333},
-		{Prefix: "+", Country: 992, Number: 000444444},
-		{Prefix: "+", Country: 7, Number: 000555555},
-	}
-		fmt.Println("Mobile Number:", number)
-}
 
 func UCard(w http.ResponseWriter, r *http.Request) {
 	var visa Card
@@ -103,7 +126,6 @@ func UCard(w http.ResponseWriter, r *http.Request) {
 	km.Transactions = KMTList
 
 	fmt.Println("Card", visa, km)
-
 }
 
 func TransferCriptoToCard(w http.ResponseWriter, r *http.Request) {
@@ -138,7 +160,6 @@ if km(6278, 5152, 5440) {
 		return
 	}
 
-//
 
 if visa(992) {
 		http.Error(w, "Wrong Card Number", http.StatusBadRequest)
@@ -156,10 +177,9 @@ func TokenizeVisaCard(cardNumber string) string {
 
 }
 
+func DropCard(w http.ResponseWriter, r *http.Request) {
 
-func Dropers(w http.ResponseWriter, r *http.Request) {
-
-	users := [2]Droper{
+	users := [2]DropCard{
 		{CardNumber: 1234567898765432},
 		{CardNumber: 5555666677778888},
 	}
